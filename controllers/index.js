7c36f254db8563ba62981b81907cb6c6f34526f1
Marina <utochkina.m96@gmail.com>
2024-07-17 18:04:48 -0400

    corrected mistakes in homeroutes and index files

--- controllers/index.js
+++ controllers/index.js
@@ -1,12 +1,7 @@
-//const express = require('express');
-//const app = express();
-
 const router = require('express').Router();
 
-const homeRoutes = require('./homeroutes');
-const apiRoutes = require('./apiroutes');
+const homeRoutes = require('./homeRoutes');
 
-router.use('/',homeRoutes);
-router.use('/api', apiRoutes)
+router.use('/', homeRoutes);
 
 module.exports = router;
