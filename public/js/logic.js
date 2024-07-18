20c26b32110eeba8a119f019927a49ea9fc41c9e
Marina <utochkina.m96@gmail.com>
2024-07-17 21:58:58 -0400

    logic.js

--- public/js/logic.js
+++ public/js/logic.js
@@ -1,16 +1,5 @@
-// Get the burger menu icon
-const burger = document.querySelector('.burger');
-
-// Get the navbar links ul
-const navbarLinks = document.querySelector('.navbar-links');
-
-// Add event listener for the burger menu icon
-burger.addEventListener('click', () => {
-    // Toggle the navbar-links class to show/hide on click
-    navbarLinks.classList.toggle('active');
-});
-
-// Optional: Close the menu when a nav link is clicked (for smoother UX)
-navbarLinks.addEventListener('click', () => {
-    navbarLinks.classList.remove('active');
-});
+const  toggleMenu = function () {
+    var navbarLinks = document.getElementById("navbarLinks");
+    navbarLinks.classList.toggle("active");
+};
+toggleMenu();
