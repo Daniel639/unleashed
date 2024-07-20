4db3d1a9f273e6d823e0077e6460b94687a011f5
Daniel Berroa <db@Daniels-MacBook-Pro.local>
2024-07-20 17:52:50 -0400

    added seeds

--- public/js/logic.js
+++ public/js/logic.js
@@ -1,12 +1,4 @@
-<<<<<<< HEAD
 function toggleMenu() {
     var navbarLinks = document.getElementById("navbarLinks");
     navbarLinks.classList.toggle("active");
 }
-=======
-const  toggleMenu = function () {
-    var navbarLinks = document.getElementById("navbarLinks");
-    navbarLinks.classList.toggle("active");
-};
-toggleMenu();
->>>>>>> bc09fba297e62e53a964be830a13459b858671c7
