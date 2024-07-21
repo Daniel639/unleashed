be45a765188347f3c356a92febb1ad7cdd62fe8a
JM344 <JAVIMARI0411@GMAIL.COM>
2024-07-21 13:17:07 -0400

    Update logic.js

--- public/js/logic.js
+++ public/js/logic.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 // Get the burger menu icon
 const burger = document.querySelector('.burger');
@@ -17,10 +16,3 @@ navbarLinks.addEventListener('click', () => {
     navbarLinks.classList.remove('active');
 });
 
-=======
-const  toggleMenu = function () {
-    var navbarLinks = document.getElementById("navbarLinks");
-    navbarLinks.classList.toggle("active");
-};
-toggleMenu();
->>>>>>> bc09fba297e62e53a964be830a13459b858671c7
