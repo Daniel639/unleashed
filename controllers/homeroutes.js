ccbfbc6ff7595a30c8e3b8049cbad72fe5f39a8b
Marina <utochkina.m96@gmail.com>
2024-07-22 18:06:50 -0400

    changes

--- controllers/homeroutes.js
+++ controllers/homeroutes.js
@@ -159,7 +159,7 @@ router.post(`/add-pet/:id`, async (req, res) => {
 
 router.get('/create-post/:id', async (req, res) => {
     try {
-        await res.render('new-post');
+        await res.render('create-post');
          } catch (err) {
         console.log("error: ", err)
          };
@@ -257,4 +257,15 @@ router.post('/logout', async (req, res) => {
         console.log("error: ", err)
     }
 });
+
 module.exports = router;
+
+router.get('/view-playdates/:id', async (req, res) => {
+    try { 
+        await res.render('scheduled-playdates', {
+        loggedIn: req.session.loggenIn
+        });
+    } catch(err) {
+        console.log("error: ", err)
+    }
+});
